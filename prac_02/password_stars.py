f3ffd86d14d8e5019a667cd2bd90047dfc56118b
Zach <zach.vanhassel@my.jcu.edu.au>
2025-10-02 02:13:35 +1000

    Refactored the program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,11 +2,29 @@
 Rewrite password star program with functions
 """
 
+#CONSTANTS
 MINIMUM_PASSWORD_LENGTH = 8
 
-password = input("Enter password: ")
-while len(password) < MINIMUM_PASSWORD_LENGTH:
-    password = input(f"Password must be greater than {MINIMUM_PASSWORD_LENGTH} characters: ")
-password_length = len(password)
-password_stars = "*" * password_length
-print(password_stars)
+def main():
+    password = get_password()
+    password_stars = convert_password_to_stars(password)
+    print(password_stars)
+
+
+def convert_password_to_stars(password: str):
+    #Function to convert the password into an equal amount of stars
+    password_length = len(password)
+    password_stars = "*" * password_length
+    return password_stars
+
+
+def get_password():
+    #Get password from user
+    password = input("Enter password: ")
+    #Check if password is longer than minimum length
+    while len(password) < MINIMUM_PASSWORD_LENGTH:
+        password = input(f"Password must be greater than {MINIMUM_PASSWORD_LENGTH} characters: ")
+    return password
+
+
+main()
