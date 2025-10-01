644fe4f987a15d505e40b2f853ce57fea28bf8e1
Zach <zach.vanhassel@my.jcu.edu.au>
2025-10-02 03:09:15 +1000

    Refactor program to use functions and added random score function

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,39 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+#import
+from random import random
+
+
+def main():
+    """Gets score from user and prints result"""
+    score = float(input("Enter score: "))
+    result = get_result(score)
+    print(result)
+
+    #Creates a random score and evaluates result
+    score = get_random_score()
+    print(f"Creating random score: {score}")
+    result = get_result(score)
+    print(result)
+
+def get_random_score():
+    """Creates a random score between 0 and 100"""
+    score = round(random() * 100,2)
+    return score
+
+
+def get_result(score: float):
+    """Get result from score"""
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
