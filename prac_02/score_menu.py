b74d8063965160e7bdb015a3a79c7c27c7ea49bb
Zach <zach.vanhassel@my.jcu.edu.au>
2025-10-02 03:44:42 +1000

    Refactored program to use menus and added more functions

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -5,23 +5,42 @@ Determining the result from score using menu
 #import
 from random import random
 
+#CONSTANT
+MENU = """(G)et a valid score
+(P)rint result
+(S)how stars
+(Q)uit"""
+
 def main():
     """Gets score from user and prints result"""
+    print(MENU)
+    choice = input(">>> ").upper()
+    score = None
+    while choice != "Q":
+        if choice == "G":
+            score = get_score()
+        elif choice == "P":
+            score_exist = check_score_exist(score)
+            if score_exist:
+                result = get_result(score)
+                print(result)
+        elif choice == "S":
+            score_exist = check_score_exist(score)
+            if score_exist:
+                stars = create_stars(score)
+                print(stars)
+        else:
+            print("Invalid Option")
+
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Goodbye")
+
+def get_score():
     score = float(input("Enter score: "))
-    result = get_result(score)
-    print(result)
-
-    #Creates a random score and evaluates result
-    score = get_random_score()
-    print(f"Creating random score: {score}")
-    result = get_result(score)
-    print(result)
-
-def get_random_score():
-    """Creates a random score between 0 and 100"""
-    score = round(random() * 100,2)
+    while score > 100 or score < 0:
+        score = float(input("Score must be between 0 and 100: "))
     return score
-
 
 def get_result(score: float):
     """Get result from score"""
@@ -34,6 +53,20 @@ def get_result(score: float):
     else:
         result = "Bad"
     return result
+
+def check_score_exist(score):
+    try:
+        float(score)
+    except TypeError:
+        score_exist = False
+        print("Score does not exist. Input score first.")
+    else:
+        score_exist = True
+    return score_exist
+
+def create_stars(score):
+    stars = "*" * int(round(score))
+    return stars
 
 
 main()
