2699191990b063deb1855679264f6e209c5b7498
Zach <zach.vanhassel@my.jcu.edu.au>
2025-10-05 21:20:00 +1000

    Refactored program to prevent Zero Division Error to occur

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -9,6 +9,8 @@ Answer the following questions:
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        denominator = int(input("Denominator cannot be 0. Enter a different denominator: "))
     fraction = numerator / denominator
     print(fraction)
 except ValueError:
