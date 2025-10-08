a2cb82b39f791ca9b47a6e8ca42001b8e44da2fe
Zach <zach.vanhassel@my.jcu.edu.au>
2025-10-08 15:07:59 +1000

    Added woefully inadequate security checker program to prac 4

---
+++ prac_04/woefully_inadequate_security_checker.py
@@ -0,0 +1,11 @@
+"""
+Check the username from the user with the authorised user list for authorisation
+"""
+
+USERNAMES = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+def main():
+    print("Does something")
+
+main()
