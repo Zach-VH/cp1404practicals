f01325ece80e1dee969d1241eb9dd9373aacc5f1
Zach <zach.vanhassel@my.jcu.edu.au>
2025-10-08 14:47:12 +1000

    Refactored code to return data to main and display subject details in a neater way

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,23 +7,27 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data(FILENAME)
-    print(data)
+    subject_details = load_data(FILENAME)
+    display_subject_details(subject_details)
 
 
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(filename)
+    data=[]
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        parts = line.strip().split(',')  # Remove the \n & Separate the data into its parts
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        data.append(parts)
     input_file.close()
+    return data
 
+def display_subject_details(subject_details):
+    subject_width = max(len(parts[0]) for parts in subject_details)
+    lecturer_width = max(len(parts[1]) for parts in subject_details)
+    student_count_width = max(len(str(parts[2])) for parts in subject_details)
+
+    for subject, lecturer, student_count in subject_details:
+        print(f"{subject:{subject_width}} is taught by {lecturer:{lecturer_width}} and has {student_count:{student_count_width}} students")
 
 main()
