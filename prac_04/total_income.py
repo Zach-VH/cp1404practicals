d385e02ee8438ec96e38eef8708918330e212214
Zach <zach.vanhassel@my.jcu.edu.au>
2025-10-08 14:28:49 +1000

    Refactored program to use functions and change variable names

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -6,19 +6,28 @@ Starter code for cumulative total income program
 
 def main():
     """Display income report for incomes over a given number of months."""
-    incomes = []
-    month = int(input("How many months? "))
+    number_of_months = int(input("How many months? "))
+    incomes = get_monthly_income(number_of_months)
+    print_income(incomes, number_of_months)
 
-    for month in range(1, month + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
-        incomes.append(income)
 
+def print_income(incomes, number_of_months):
+    """Print income for every month given"""
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, month + 1):
+    for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
+
+
+def get_monthly_income(number_of_months):
+    """Gets monthly income from user for each month"""
+    incomes = []
+    for month in range(1, number_of_months + 1):
+        income = float(input(f"Enter income for month {month}: "))
+        incomes.append(income)
+    return incomes
 
 
 main()
