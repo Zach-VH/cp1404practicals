83042e2ef0a696e20af0de0f7b997364fd3d0ce2
Zach <zach.vanhassel@my.jcu.edu.au>
2025-10-12 01:59:53 +1000

    Refactored code to use enumerate to print income

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -8,15 +8,14 @@ def main():
     """Display income report for incomes over a given number of months."""
     number_of_months = int(input("How many months? "))
     incomes = get_monthly_income(number_of_months)
-    print_income(incomes, number_of_months)
+    print_income(incomes)
 
 
-def print_income(incomes, number_of_months):
+def print_income(incomes):
     """Print income for every month given"""
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
-        income = incomes[month - 1]
+    for month, income in enumerate(incomes,1):
         total += income
         print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
 
