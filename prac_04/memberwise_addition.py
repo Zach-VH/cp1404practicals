d008c5d4d51742d84c5f96c52afe8e3f07c03290
Zach <zach.vanhassel@my.jcu.edu.au>
2025-10-12 03:15:49 +1000

    Refactored code to check if list contains a string and sends an error

--- prac_04/memberwise_addition.py
+++ prac_04/memberwise_addition.py
@@ -3,8 +3,8 @@ CP1404 - memberwise addition
 Gets Two Lists and adds the lists together
 """
 
-LIST_1 = [1,2,3]
-LIST_2 = [4,5,6,4]
+LIST_1 = [1.0,2,3,0]
+LIST_2 = [4.5,5.5,3,0]
 
 def main():
     print(memberwise_addition(LIST_1,LIST_2))
@@ -12,9 +12,12 @@ def main():
 def memberwise_addition(a,b):
     output = []
     if len(a) == len(b):
-        for i in range(0,len(a)):
-            output.append(a[i]+b[i])
-        return output
+        try:
+            for i in range(0,len(a)):
+                output.append(a[i]+b[i])
+            return output
+        except TypeError:
+            return "Error: List contains value other than int or float"
     else:
         return "Error: Length of both list are different"
 
