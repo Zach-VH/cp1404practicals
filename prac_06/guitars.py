b42ba828cc45f18118d21935d1d9538260a315f2
Zach <zach.vanhassel@my.jcu.edu.au>
2025-10-29 15:12:30 +1000

    Refactor program to sanitise user inputs

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -2,6 +2,7 @@
 Guitars list
 Estimate: 22 minutes
 Actual: 28 minutes
+
 """
 from prac_06.guitar import Guitar
 
@@ -24,12 +25,37 @@ def get_guitars():
     guitars = []
     name = input("Name: ")
     while name != "":
-        year = year = int(input("Year: "))
-        cost = round(float(input("Cost: $")), 2)
+        year = get_year()
+        cost = get_cost()
         guitars.append(Guitar(name, year, cost))
         print(guitars[-1])
         name = input("\nName: ")
     return guitars
 
+
+def get_cost() -> float:
+    cost = -1
+    while cost == -1:
+        try:
+            cost = round(float(input("Cost: $")), 2)
+            if cost <= 0:
+                print("Year must be greater than 0")
+                cost = -1
+        except ValueError:
+            print("Year must be a decimal number")
+    return cost
+
+
+def get_year():
+    year = -1
+    while year == -1:
+        try:
+            year = int(input("Year: "))
+            if year <= 0 or year > 2025:
+                print("Year must be greater than 0 and less than 2025")
+                year = -1
+        except ValueError:
+            print("Year must be a whole number")
+    return year
 
 main()
