ed6d538f76102d7a715c25b2bf63e963a58f5eca
Zach <zach.vanhassel@my.jcu.edu.au>
2025-10-29 14:27:48 +1000

    Refactor program to complete task required

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -4,3 +4,21 @@ Estimated: 15 minutes
 Actual:
 """
 
+class Guitar:
+    """A representation of a guitar object"""
+    def __init__(self, name="",year=0,cost=0.0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : ${self.cost:.2f}"
+
+    def get_age(self):
+        return 2025 - self.year
+
+    def is_vintage(self):
+        if self.get_age() >= 50:
+            return True
+        else:
+            return False
--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,3 +1,15 @@
 """
 Test area for guitar class
 """
+
+from guitar import Guitar
+
+guitars = [Guitar("Gibson L-5 CES",1922,16035.40),
+           Guitar("Another Guitar",1975,16035.40)]
+
+print(f"{guitars[0].name} get_age() - Expected 103. Got {guitars[0].get_age()}.")
+print(f"{guitars[1].name} get_age() - Expected 5. Got {guitars[1].get_age()}.")
+print(f"{guitars[0].name} is_vintage() - Expected True. Got {guitars[0].is_vintage()}.")
+print(f"{guitars[1].name} is_vintage() - Expected True. Got {guitars[1].is_vintage()}. (Edge Case)")
+
+
