3d27ed9f126082db3d5382039c05d3eeb5bb1f3a
Zach <zach.vanhassel@my.jcu.edu.au>
2025-10-29 15:03:07 +1000

    Refactor program to complete task required

---
+++ prac_06/guitars.py
@@ -0,0 +1,35 @@
+"""
+Guitars list
+Estimate: 22 minutes
+Actual: 28 minutes
+"""
+from prac_06.guitar import Guitar
+
+def main():
+    print("My guitars!")
+    guitars = get_guitars()
+    print_guitar_list(guitars)
+
+
+def print_guitar_list(guitars):
+    print("\nThese are my guitars:")
+    max_name_width = max(len(guitar.name) for guitar in guitars)
+    max_cost_width = max(len(str(guitar.cost)) for guitar in guitars)
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (is vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i} : {guitar.name:>{max_name_width}} ({guitar.year:4}), worth $ {guitar.cost:<{max_cost_width}}{vintage_string}")
+
+
+def get_guitars():
+    guitars = []
+    name = input("Name: ")
+    while name != "":
+        year = year = int(input("Year: "))
+        cost = round(float(input("Cost: $")), 2)
+        guitars.append(Guitar(name, year, cost))
+        print(guitars[-1])
+        name = input("\nName: ")
+    return guitars
+
+
+main()
