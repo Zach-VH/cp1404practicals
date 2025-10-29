9e8ace503ff12843131ea05384bc4f2de25584f0
Zach <zach.vanhassel@my.jcu.edu.au>
2025-10-29 13:31:09 +1000

    Refactor code to be able to print string of object and add 'name' to object instance

--- prac_06/car.py
+++ prac_06/car.py
@@ -4,13 +4,21 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
 
+        name: float, name/model of the car
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name
         self.fuel = fuel
         self._odometer = 0
+
+    def __str__(self):
+        """Creates string if called by str() function
+        Returning string 'Car, fuel=unit, odometer=km'
+        """
+        return f"Car, fuel={self.fuel}, odometer={self._odometer}"
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
