c5da5ccc8c7dbb304f6bd6c5592dce5e2194a6b2
Zach <zach.vanhassel@my.jcu.edu.au>
2025-10-29 13:32:30 +1000

    Refactor code to add name to car object

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,16 +9,21 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("My Car", 180)
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
 
-    limo = Car(100)
+    """"""
+    limo = Car("Limo",100)
     print(f"Limo currently has {limo.fuel} units of fuel")
     print("Filling 20 units of fuel into limo")
     limo.add_fuel(20)
     print(f"Limo currently has {limo.fuel} units of fuel")
+    print(f"Attempting to drive 115 km in limo")
+    limo.drive(115)
+    print(f"Limo currently has {limo.fuel} units of fuel")
+    print(limo)
 
 
 
