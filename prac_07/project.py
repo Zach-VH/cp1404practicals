09cf5b9951fed9c3b4f046e880a3fd68f70b0417
Zach <zach.vanhassel@my.jcu.edu.au>
2025-11-09 22:20:21 +1000

    Refactored code to display projects

---
+++ prac_07/project.py
@@ -0,0 +1,34 @@
+"""
+Project Class
+"""
+
+class Project:
+    def __init__(self, name="",date="",priority=0,cost=0.0,completion=0.0):
+        self.name = name
+        self.date = date
+        self.priority = priority
+        self.cost = cost
+        self.completion = completion
+
+    def __repr__(self):
+        return (f"{self.name}, start:{self.date}, priority {self.priority}, estimated: ${self.cost:.2f}, "
+                f"completion: {self.completion * 100:.2f}%")
+
+    def is_complete(self):
+        if self.completion == 1:
+            return True
+        else:
+            return False
+
+def run_tests():
+    projects = []
+    with open("project.txt", 'r') as in_file:
+        header = in_file.readline().strip()
+        for line in in_file:
+            parts = line.strip().split(',')
+            project = Project(parts[0], parts[1], int(parts[2]), float(parts[3]), float(parts[4]))
+            projects.append(project)
+    print(projects)
+    print(f"Completed:{[project for project in projects if project.is_complete()]}")
+
+run_tests()
---
+++ prac_07/project_management.py
@@ -0,0 +1,69 @@
+"""
+
+Estimate: 80 minutes
+
+Code:
+Documentation:
+Actual:
+"""
+from project import Project
+
+DEFAULT_FILENAME = "project.txt"
+MENU = """- (L)oad projects
+- (S)ave projects
+- (D)isplay projects
+- (F)ilter projects by date
+- (A)dd new project
+- (U)pdate project
+- (Q)uit"""
+
+def main():
+    projects, header = load_projects(DEFAULT_FILENAME)
+    print(f"Loaded {len(projects)} projects from {DEFAULT_FILENAME}")
+    #display_projects(projects)
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            print("Load selected")
+        elif choice == "S":
+            print("Save selected")
+        elif choice == "D":
+            display_projects(projects)
+        elif choice == "F":
+            print("Filter selected")
+        elif choice == "A":
+            print("Add selected")
+        elif choice == "U":
+            print("Update selected")
+        else:
+            print("Invalid Input")
+        print(MENU)
+        choice = input(">>> ").upper()
+
+
+def load_projects(filename):
+    projects = []
+    with open(filename, 'r') as in_file:
+        header = in_file.readline().strip()
+        for line in in_file:
+            parts = line.strip().split(',')
+            project = Project(parts[0],parts[1],int(parts[2]),float(parts[3]),float(parts[4]))
+            projects.append(project)
+    return projects, header
+
+def display_projects(projects):
+    print("Incomplete projects:")
+    for project in projects:
+        if not project.is_complete():
+            print(f"  {project}")
+    print("Complete projects:")
+    for project in projects:
+        if project.is_complete():
+            print(f"  {project}")
+
+def update_project(projects):
+    for projects, i in enumerate(projects,0):
+        print(f"{i} {projects}")
+
+main()
