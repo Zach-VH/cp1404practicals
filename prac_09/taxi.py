875ef48df3006f3232026ebdd6d0e53aa8d83a10
Zach <zach.vanhassel@my.jcu.edu.au>
2025-11-23 17:34:46 +1000

    Remove unnecessary line

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -10,7 +10,6 @@ class Taxi(Car):
     price_per_km = 1.23
     def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
-        price_per_km = 1.23
         super().__init__(name, fuel)
         self.current_fare_distance = 0
 
