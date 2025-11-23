1a919e2965e01a3118c66ae14f296f30435a523f
Zach <zach.vanhassel@my.jcu.edu.au>
2025-11-23 18:35:53 +1000

    Add Silver Service Taxi test program with tests

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,12 @@
+"""
+Prac 9
+Silver Service Taxi Class Test
+"""
+from silver_service_taxi import SilverServiceTaxi
+
+my_taxi = SilverServiceTaxi('Hummer',200,2)
+print(my_taxi)
+
+my_taxi.drive(18)
+# print(f"Fare: ${my_taxi.get_fare()}")
+assert my_taxi.get_fare() == 48.78
